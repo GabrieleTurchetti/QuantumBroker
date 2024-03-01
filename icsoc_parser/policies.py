593269e2361f03089f2635783282afaed2c9a1c3
GabrieleTurchetti <gabriele.turchetti02@gmail.com>
2024-03-01 19:45:59 +0100

    feat: New policies managment

--- icsoc_parser/policies.py
+++ icsoc_parser/policies.py
@@ -1,27 +1,3 @@
-import math
-
-# Lowcost policy
-def lowcost(dispatches, total_shots, level):
-    percentage = 100 - level
-    new_size = math.ceil((len(dispatches) * percentage) / 100)
-    dispatches.sort(key = lambda dispatch: list(dispatch["total_cost"])[0][0])
-    return dispatches[:new_size]
-
-# Fast policy
-def fast(dispatches, total_shots, level):
-    percentage = 100 - level
-    new_size = math.ceil((len(dispatches) * percentage) / 100)
-    dispatches.sort(key = lambda dispatch: list(dispatch["total_time"])[0][0])
-    return dispatches[:new_size]
-
-# Accurate policy
-def accurate(dispatches, total_shots, level):
-    percentage = 100 - level
-    new_size = math.ceil((len(dispatches)* percentage) / 100)
-    dispatches.sort(key = lambda dispatch: len(dispatch["dispatch"]), reverse = True)
-    return dispatches[:new_size]
-
-# Fair policy
 def get_deviation(dispatch, total_shots):
     avg = total_shots / len(dispatch["dispatch"])
     dev = 0
@@ -31,23 +7,31 @@ def get_deviation(dispatch, total_shots):
 
     return dev
 
-def fair(dispatches, total_shots, level):
-    percentage = 100 - level
-    new_size = math.ceil((len(dispatches) * percentage) / 100)
-    dispatches.sort(key = lambda dispatch: get_deviation(dispatch, total_shots))
-    return dispatches[:new_size]
-
-# Balanced policy
-def get_balancing(dispatch, total_shots):
-    total_cost = list(dispatch["total_cost"])[0][0]
-    total_time = list(dispatch["total_time"])[0][0]
-    used_backends = len(dispatch["dispatch"])
+def get_dispatch_value(dispatch, shots, weights, min_values, max_values):
+    cost_weight, time_weight, accuracy_weight, uniformity_weight = weights
+    cost = list(dispatch["total_cost"])[0][0]
+    time = list(dispatch["total_time"])[0][0]
+    used_computers = len(dispatch["dispatch"])
     deviation = get_deviation(dispatch, total_shots)
-    balancing = - total_cost * 10 - total_time + used_backends * 100 - deviation
-    return balancing
+    normalized_cost = (cost - min_values[0]) / (max_values[0] - min_values[0])
+    normalized_time = (cost - min_values[1]) / (max_values[1] - min_values[1])
+    normalized_used_computers = (cost - min_values[2]) / (max_values[2] - min_values[3])
+    normalized_deviation = (cost - min_values[3]) / (max_values[3] - min_values[3])
+    return normalized_cost * cost_weight + normalized_time * time_weight + nomalized_used_computers * accuracy_weight + normalized_deviation * uniformity_weight
 
-def balanced(dispatches, total_shots, level):
+
+def standard(dispatches, shots, weights, level):
+    min_cost = min(dispatches, key = lambda dispatch: list(dispatch["total_cost"])[0][0])
+    min_time = min(dispatches, key = lambda dispatch: list(dispatch["total_time"])[0][0])
+    min_used_computers = min(dispatches, key = lambda dispatch: len(dispatch["dispatch"]))
+    min_deviation = min(dispatches, key = lambda dispatch: get_deviation(dispatch, total_shots))
+    max_cost = max(dispatches, key = lambda dispatch: list(dispatch["total_cost"])[0][0])
+    max_time = max(dispatches, key = lambda dispatch: list(dispatch["total_time"])[0][0])
+    max_used_computers = max(dispatches, key = lambda dispatch: len(dispatch["dispatch"]))
+    max_deviation = max(dispatches, key = lambda dispatch: get_deviation(dispatch, total_shots))
+    min_values = (min_cost, min_time, min_used_computers, min_deviation)
+    max_values = (max_cost, max_time, max_used_computers, max_deviation)
     percentage = 100 - level
     new_size = math.ceil((len(dispatches) * percentage) / 100)
-    dispatches.sort(key = lambda dispatch: get_balancing(dispatch, total_shots), reverse = True)
+    dispatches.sort(key = lambda dispatch: get_dispatch_value(dispatch, shots, weights, min_values, max_values), reverse = True)
     return dispatches[:new_size]
---
+++ icsoc_parser/policies_old.py
@@ -0,0 +1,53 @@
+import math
+
+# Lowcost policy
+def lowcost(dispatches, total_shots, level): # cost aware
+    percentage = 100 - level
+    new_size = math.ceil((len(dispatches) * percentage) / 100)
+    dispatches.sort(key = lambda dispatch: list(dispatch["total_cost"])[0][0])
+    return dispatches[:new_size]
+
+# Fast policy
+def fast(dispatches, total_shots, level): # time aware
+    percentage = 100 - level
+    new_size = math.ceil((len(dispatches) * percentage) / 100)
+    dispatches.sort(key = lambda dispatch: list(dispatch["total_time"])[0][0])
+    return dispatches[:new_size]
+
+# Accurate policy
+def accurate(dispatches, total_shots, level): # to change
+    percentage = 100 - level
+    new_size = math.ceil((len(dispatches)* percentage) / 100)
+    dispatches.sort(key = lambda dispatch: len(dispatch["dispatch"]), reverse = True)
+    return dispatches[:new_size]
+
+# Fair policy
+def get_deviation(dispatch, total_shots):
+    avg = total_shots / len(dispatch["dispatch"])
+    dev = 0
+
+    for _, _, shots in dispatch["dispatch"]:
+        dev += abs(shots - avg)
+
+    return dev
+
+def fair(dispatches, total_shots, level): # uniform
+    percentage = 100 - level
+    new_size = math.ceil((len(dispatches) * percentage) / 100)
+    dispatches.sort(key = lambda dispatch: get_deviation(dispatch, total_shots))
+    return dispatches[:new_size]
+
+# Balanced policy
+def get_balancing(dispatch, total_shots):
+    total_cost = list(dispatch["total_cost"])[0][0]
+    total_time = list(dispatch["total_time"])[0][0]
+    used_backends = len(dispatch["dispatch"])
+    deviation = get_deviation(dispatch, total_shots)
+    balancing = - total_cost * 10 - total_time + used_backends * 100 - deviation
+    return balancing
+
+def balanced(dispatches, total_shots, level):
+    percentage = 100 - level
+    new_size = math.ceil((len(dispatches) * percentage) / 100)
+    dispatches.sort(key = lambda dispatch: get_balancing(dispatch, total_shots), reverse = True)
+    return dispatches[:new_size]
