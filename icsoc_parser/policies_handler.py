a184a1a42f8d5683104e8d31a3b280e1bb7a27f0
GabrieleTurchetti <gabriele.turchetti02@gmail.com>
2024-03-05 00:13:59 +0100

    feat: eco_sustainability weight in policies

--- icsoc_parser/policies_handler.py
+++ icsoc_parser/policies_handler.py
@@ -2,10 +2,19 @@ from policy_handler import filter_dispatches_by_policy
 
 def policy_is_valid(policy):
     try:
-        if policy["cost_weight"] < 0 or policy["time_weight"] < 0 or policy["reliability_weight"] < 0 or policy["uniformity_weight"] < 0:
+        if policy["weights"]["cost"] < -1 or policy["weights"]["cost"] > 1:
             return False
 
-        if not (policy["cost_weight"] + policy["time_weight"] + policy["reliability_weight"] + policy["uniformity_weight"] == 1):
+        if policy["weights"]["eco_sustainability"] < -1 or policy["weights"]["eco_sustainability"] > 1:
+            return False
+
+        if policy["weights"]["time"] < -1 or policy["weights"]["time"] > 1:
+            return False
+
+        if policy["weights"]["reliability"] < -1 or policy["weights"]["reliability"] > 1:
+            return False
+
+        if policy["weights"]["uniformity"] < -1 or policy["weights"]["uniformity"] > 1:
             return False
 
         if policy["level"] < 1 or policy["level"] > 99:
@@ -28,10 +37,16 @@ def filter_dispatches_by_policies(dispatches, total_shots, policies):
         if len(new_dispatches) == 1:
             break
             
-        new_dispatches = filter_dispatches_by_policy(new_dispatches, total_shots, (policy["cost_weight"], policy["time_weight"], policy["reliability_weight"], policy["uniformity_weight"]), policy["level"])
+        new_dispatches = filter_dispatches_by_policy(new_dispatches, total_shots, policy["weights"], policy["level"])
 
     if len(new_dispatches) > 1:
-        new_dispatches = filter_dispatches_by_policy(new_dispatches, total_shots, (0.25, 0.25, 0.25, 0.25), 99)
+        new_dispatches = filter_dispatches_by_policy(new_dispatches, total_shots, {
+            "cost": 1,
+            "eco_sustainability": 1,
+            "time": 1,
+            "reliability": 1,
+            "uniformity": 1
+        }, 99)
 
     dispatch = new_dispatches[0]
     return dispatch
--- icsoc_parser/policy_handler.py
+++ icsoc_parser/policy_handler.py
@@ -10,30 +10,57 @@ def get_dispatch_standard_deviation(dispatch, total_shots):
     deviation = math.sqrt(sum / len(dispatch["dispatch"]))
     return deviation
 
+def get_dispatch_used_computers(dispatch):
+    length = 0
+
+    for _, _, shots in dispatch["dispatch"]:
+        if shots != 0:
+            length += 1
+
+    return length
+
 def get_dispatch_value(dispatch, total_shots, weights, min_values, max_values):
-    cost_weight, time_weight, reliability_weight, uniformity_weight = weights
     cost = list(dispatch["total_cost"])[0][0]
+    energy_cost = list(dispatch["total_energy_cost"])[0][0]
     time = list(dispatch["total_time"])[0][0]
-    used_computers = len(dispatch["dispatch"])
+    used_computers = get_dispatch_used_computers(dispatch)
     deviation = get_dispatch_standard_deviation(dispatch, total_shots)
-    normalized_cost = (cost - min_values[0]) / ((max_values[0] - min_values[0]) if (max_values[0] - min_values[0]) != 0 else 1)
-    normalized_time = (time - min_values[1]) / ((max_values[1] - min_values[1]) if (max_values[1] - min_values[1]) != 0 else 1)
-    normalized_used_computers = (used_computers - min_values[2]) / ((max_values[2] - min_values[2]) if (max_values[2] - min_values[2]) != 0 else 1)
-    normalized_deviation = (deviation - min_values[3]) / ((max_values[3] - min_values[3]) if (max_values[3] - min_values[3]) != 0 else 1)
-    value = - normalized_cost * cost_weight - normalized_time * time_weight + normalized_used_computers * reliability_weight - normalized_deviation * uniformity_weight
+    normalized_cost = (cost - min_values["cost"]) / ((max_values["cost"] - min_values["cost"]) if (max_values["cost"] - min_values["cost"]) != 0 else 1)
+    normalized_energy_cost = (energy_cost - min_values["energy_cost"]) / ((max_values["energy_cost"] - min_values["energy_cost"]) if (max_values["energy_cost"] - min_values["energy_cost"]) != 0 else 1)
+    normalized_time = (time - min_values["time"]) / ((max_values["time"] - min_values["time"]) if (max_values["time"] - min_values["time"]) != 0 else 1)
+    normalized_used_computers = (used_computers - min_values["used_computers"]) / ((max_values["used_computers"] - min_values["used_computers"]) if (max_values["used_computers"] - min_values["used_computers"]) != 0 else 1)
+    normalized_deviation = (deviation - min_values["deviation"]) / ((max_values["deviation"] - min_values["deviation"]) if (max_values["deviation"] - min_values["deviation"]) != 0 else 1)
+    value = - normalized_cost * weights["cost"] - normalized_energy_cost * weights["eco_sustainability"] - normalized_time * weights["time"] + normalized_used_computers * weights["reliability"] - normalized_deviation * weights["uniformity"]
     return value
 
 def filter_dispatches_by_policy(dispatches, total_shots, weights, level):
     min_cost = list(min(dispatches, key = lambda dispatch: list(dispatch["total_cost"])[0][0])["total_cost"])[0][0]
+    min_energy_cost = list(min(dispatches, key = lambda dispatch: list(dispatch["total_energy_cost"])[0][0])["total_energy_cost"])[0][0]
     min_time = list(min(dispatches, key = lambda dispatch: list(dispatch["total_time"])[0][0])["total_time"])[0][0]
-    min_used_computers = len(min(dispatches, key = lambda dispatch: len(dispatch["dispatch"]))["dispatch"])
+    min_used_computers = get_dispatch_used_computers(min(dispatches, key = lambda dispatch: len(dispatch["dispatch"])))
     min_deviation = get_dispatch_standard_deviation(min(dispatches, key = lambda dispatch: get_dispatch_standard_deviation(dispatch, total_shots)), total_shots)
     max_cost = list(max(dispatches, key = lambda dispatch: list(dispatch["total_cost"])[0][0])["total_cost"])[0][0]
+    max_energy_cost = list(max(dispatches, key = lambda dispatch: list(dispatch["total_energy_cost"])[0][0])["total_energy_cost"])[0][0]
     max_time = list(max(dispatches, key = lambda dispatch: list(dispatch["total_time"])[0][0])["total_time"])[0][0]
-    max_used_computers = len(max(dispatches, key = lambda dispatch: len(dispatch["dispatch"]))["dispatch"])
+    max_used_computers = get_dispatch_used_computers(max(dispatches, key = lambda dispatch: len(dispatch["dispatch"])))
     max_deviation = get_dispatch_standard_deviation(max(dispatches, key = lambda dispatch: get_dispatch_standard_deviation(dispatch, total_shots)), total_shots)
-    min_values = (min_cost, min_time, min_used_computers, min_deviation)
-    max_values = (max_cost, max_time, max_used_computers, max_deviation)
+
+    min_values = {
+        "cost": min_cost,
+        "energy_cost": min_energy_cost,
+        "time": min_time,
+        "used_computers": min_used_computers,
+        "deviation": min_deviation
+    }
+
+    max_values = {
+        "cost": max_cost,
+        "energy_cost": max_energy_cost,
+        "time": max_time,
+        "used_computers": max_used_computers,
+        "deviation": max_deviation
+    }
+    
     percentage = 100 - level
     new_size = math.ceil((len(dispatches) * percentage) / 100)
     dispatches.sort(key = lambda dispatch: get_dispatch_value(dispatch, total_shots, weights, min_values, max_values), reverse = True)
