5e0ead4b6dfb9e452d937a60fce823b81a2691ca
GabrieleTurchetti <gabriele.turchetti02@gmail.com>
2024-04-29 10:10:03 +0200

    fix: Distribution standard policies

--- icsoc_parser/distribution/standard_policies.py
+++ icsoc_parser/distribution/standard_policies.py
@@ -1,6 +1,27 @@
 # equal policy
 def get_distribution_by_equal_policy(results):
     total_distribution = {}
+    results_shots = {}
+    total_shots = 0
+
+    for result in results:
+        result = result.to_dict()
+        total_shots += result["shots"]
+
+        for key, value in result["data"].items():
+            if key in results_shots:
+                results_shots[key] += value
+            else:
+                results_shots[key] = value
+
+    for key, value in results_shots.items():
+        total_distribution[key] = f"{round(value * 100 / total_shots, 2)}%"
+
+    return total_distribution
+
+# fair policy
+def get_distribution_by_fair_policy(results):
+    total_distribution = {}
     partial_distributions_sum = {}
 
     for result in results:
@@ -14,33 +35,5 @@ def get_distribution_by_equal_policy(results):
 
     for key, value in partial_distributions_sum.items():
         total_distribution[key] = f"{round(value * 100 / len(results), 2)}%"
-
-    return total_distribution
-
-def get_partial_distribution(result):
-    result = result.to_dict()
-    partial_distribution = {}
-
-    for key, value in result["data"].items():
-        partial_distribution[key] = value * 100 / result["shots"]
-
-    return partial_distribution
-
-# fair policy
-def get_distribution_by_fair_policy(results):
-    total_distribution = {}
-    partial_distributions_sum = {}
-
-    for result in results:
-        partial_distribution = get_partial_distribution(result)
-
-        for key, value in partial_distribution.items():
-            if key in partial_distributions_sum:
-                partial_distributions_sum[key] += value
-            else:
-                partial_distributions_sum[key] = value
-
-    for key, value in partial_distributions_sum.items():
-        total_distribution[key] = f"{round(value / len(results), 2)}%"
 
     return total_distribution
